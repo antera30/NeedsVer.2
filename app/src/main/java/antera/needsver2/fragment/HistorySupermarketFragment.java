f6d08346db2f8c165d9fe04414ddad09b3c63207
antera30 <fajariyawan@gmail.com>
2017-05-12 10:40:06 +0700

    Update 4.7

--- app/src/main/java/antera/needsver2/fragment/HistorySupermarketFragment.java
+++ app/src/main/java/antera/needsver2/fragment/HistorySupermarketFragment.java
@@ -35,6 +35,7 @@ public class HistorySupermarketFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
